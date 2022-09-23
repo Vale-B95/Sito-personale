9a376e88d0d23383bd65fde0500b3b33771db058
Valentina Bernacchia <vbernacchia02@gmail.com>
2022-09-23 11:58:41 +0200

    editing and deleting unnecessary files

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+/* import logo from './logo.svg';
+ */import './App.css';
+import React from 'react';
+
+function App() {
+
+  let ciao="I'm React";
+  let prova =`Hello Word, ${ciao}`
+
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        {prova}
+      </header>
+    </div>
+  );
+}
+
+export default App;
